b128a3f39a4d1097e433d2388f15a7b43c8da8e6
Rus Hughes <russell.hughes@gmail.com>
2017-10-22 20:12:47 +0700

    https://www.udemy.com/react-redux/ chapter 4 lecture 4 completed

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
 
   renderList() {
     return this.props.books.map((book) => {
@@ -18,3 +19,13 @@ export default class BookList extends Component {
     );
   }
 }
+
+function mapStateToProps(state) {
+  // whatever is returned iwll show up as props
+  // inside of BookList
+  return {
+    books: state.books
+  };
+}
+
+export default connect(mapStateToProps)(BookList);
